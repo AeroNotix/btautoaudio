c1d89afa6338774799a0aee3cd035fecf74f362c
Aaron France <aaron.l.france@gmail.com>
2017-07-03 11:11:32 +0200

    Rewrite logging

--- create_a2dp_sinks.py
+++ create_a2dp_sinks.py
@@ -16,14 +16,14 @@ def extract_bt_addr(path):
     return "_".join(path.split('/')[-1].split('_')[1:])
 
 
-def add_bluetooth_audio_source(path, retry_timeout=1, retries=0, give_up=3):
+def add_bluetooth_audio_source(path, retry_timeout=1, retries=0, give_up=5):
     if retries == give_up:
         return
     cmd = 'pactl load-module module-loopback source_dont_move=yes source=bluez_source.%s.a2dp_source sink=%s'
     bt_addr = extract_bt_addr(path)
-    journal.write("Found: %s attempting to create interface" % bt_addr)
+    journal.send("Found: %s attempting to create interface" % bt_addr)
     if os.system(cmd % (bt_addr, expected_sink)) != 0:
-        journal.write("Failed to add loopback for %s. Waiting: %ds" % (bt_addr, retry_timeout))
+        journal.send("Failed to add loopback for %s. Waiting: %ds" % (bt_addr, retry_timeout))
         time.sleep(retry_timeout)
         add_bluetooth_audio_source(path, retry_timeout=retry_timeout*2, retries=retries+1)
 
@@ -51,12 +51,12 @@ def property_changed(interface, changed, invalidated, path=None):
         remove_bluetooth_audio_source(path)
 
 if __name__ == '__main__':
-    journal.write("Starting to automatically monitor for BT Audio sources")
+    journal.send("Starting to automatically monitor for BT Audio sources")
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
     bus = dbus.SystemBus()
 
-    journal.write("Automatically creating a2dp sinks from BT Audio sources")
+    journal.send("Automatically creating a2dp sinks from BT Audio sources")
     bus.add_signal_receiver(
         property_changed,
         bus_name="org.bluez",
